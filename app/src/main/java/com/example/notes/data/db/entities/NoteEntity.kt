6affc62f82eb49456daff514ba15dea2a2dffa7b
pokorpe <117550576+pokorpe@users.noreply.github.com>
2023-04-30 04:49:04 +0200

    Frontend baseline

---
+++ app/src/main/java/com/example/notes/data/db/entities/NoteEntity.kt
@@ -0,0 +1,12 @@
+package com.example.notes.data.db.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class NoteEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0L,
+    val title: String = "",
+    val text: String = "",
+)
