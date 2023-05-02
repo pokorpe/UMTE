cb838149a1576337b8f6bc659c0dd5850e613d19
pokorpe <117550576+pokorpe@users.noreply.github.com>
2023-05-03 01:18:01 +0200

    Img cleaned + bugfix

--- app/src/main/java/com/example/notes/AddNote.kt
+++ app/src/main/java/com/example/notes/AddNote.kt
@@ -9,16 +9,16 @@ import android.graphics.ImageDecoder
 import android.icu.text.SimpleDateFormat
 import android.net.Uri
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.view.Window
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.notes.data.db.entities.NoteEntity
 import com.example.notes.databinding.ActivityAddNoteBinding
 import com.nvt.color.ColorPickerDialog
-import java.lang.Exception
 import java.util.*
 
 class AddNote : AppCompatActivity() {
@@ -26,47 +26,44 @@ class AddNote : AppCompatActivity() {
     private lateinit var binding: ActivityAddNoteBinding
 
     private lateinit var note: NoteEntity
-    private lateinit var old_note: NoteEntity
-    var isUpdated = false
+    private lateinit var oldNote: NoteEntity
+    private var isUpdated = false
+    private var pictureUri = ""
 
-    //var pickedPhoto: Uri? = null
-    //var pickedBitMap: Bitmap? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        supportActionBar?.hide()
         binding = ActivityAddNoteBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         try {
             //TODO
-            old_note = intent.getSerializableExtra("current_note") as NoteEntity
-            binding.etTitle.setText(old_note.title)
-            binding.etNote.setText(old_note.note)
-            //!!!
-            binding.etTitle.setTextColor(old_note.titleColor)
+            oldNote = intent.getSerializableExtra("current_note") as NoteEntity
+            binding.etTitle.setText(oldNote.title)
+            binding.etNote.setText(oldNote.note)
+
+            binding.etTitle.setTextColor(oldNote.titleColor)
             isUpdated = true
 
-            binding.etTemp.setText(old_note.pictureUri)
-            binding.pictureField.setImageBitmap(decode(Uri.parse(old_note.pictureUri)))
-            // decode(Uri.parse(old_note.pictureUri))
-
+            pictureUri = oldNote.pictureUri
+            binding.pictureField.setImageBitmap(decode(Uri.parse(oldNote.pictureUri)))
 
         } catch (e: Exception) {
             e.printStackTrace()
         }
-        // var tc = binding.etTitle.currentTextColor
 
         binding.imgColor.setOnClickListener {
             val colorPicker = ColorPickerDialog(
                 this,
-                Color.BLACK, // color init
-                false, // true is show alpha
+                Color.BLACK,
+                true,
                 object : ColorPickerDialog.OnColorPickerListener {
                     override fun onCancel(dialog: ColorPickerDialog?) {
                     }
 
                     override fun onOk(dialog: ColorPickerDialog?, colorPicker: Int) {
-                        // tc = colorPicker
                         binding.etTitle.setTextColor(colorPicker)
                     }
                 })
@@ -81,8 +78,9 @@ class AddNote : AppCompatActivity() {
             ) {
                 ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1)
             } else {
-                val galleryInText = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-                startActivityForResult(galleryInText, 2)
+                val selectPicture = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                val chooser = Intent.createChooser(selectPicture,"")
+                startActivityForResult(chooser, 2)
             }
         }
 
@@ -90,18 +88,15 @@ class AddNote : AppCompatActivity() {
             val title = binding.etTitle.text.toString()
             val noteDesc = binding.etNote.text.toString()
             val tc = binding.etTitle.currentTextColor
-            val uri = binding.etTemp.text.toString()
-            //val uri = note.pictureUri
+            val uri = pictureUri
             if (title.isNotEmpty() || noteDesc.isNotEmpty()) {
                 val formatter = SimpleDateFormat("EEE, d MMM yyyy HH:mm")
                 if (isUpdated) {
-                    //val uri = old_note.pictureUri
                     note = NoteEntity(
-                        old_note.id, title, noteDesc, old_note.date, tc, uri
+                        oldNote.id, title, noteDesc, oldNote.date, tc, uri
                     )
                     println("1")
                 } else {
-                    //val uri = note.pictureUri
                     note = NoteEntity(
                         null, title, noteDesc, formatter.format(Date()), tc, uri
                     )
@@ -119,6 +114,15 @@ class AddNote : AppCompatActivity() {
         binding.imgBackArrow.setOnClickListener {
             onBackPressedDispatcher.onBackPressed()
         }
+        binding.pictureField.setOnClickListener {
+            val openPicture = Intent(Intent.ACTION_VIEW, Uri.parse(pictureUri))
+           // val chooser = Intent.createChooser(openPicture, pictureUri)
+            startActivity(openPicture)
+           // startActivity(chooser )
+            //Toast.makeText(this@AddNote, pictureUri, Toast.LENGTH_LONG).show()
+            //binding.etNote.setText(pictureUri)
+        }
+
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
@@ -132,22 +136,6 @@ class AddNote : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (requestCode == 2 && resultCode == Activity.RESULT_OK && data != null) {
             decode(data.data)
-            /*
-            var pickedPhoto = data.data
-            var pickedBitMap: Bitmap? = null
-            println("photo: " + pickedPhoto)
-
-                        if (Build.VERSION.SDK_INT >= 28) {
-                            val source = ImageDecoder.createSource(this.contentResolver, pickedPhoto!!)
-                            pickedBitMap = ImageDecoder.decodeBitmap(source)
-                            println("bitmap: "+pickedBitMap)
-                            binding.pictureField.setImageBitmap(pickedBitMap)
-                            println(pickedBitMap)
-                        } else {
-                            pickedBitMap = MediaStore.Images.Media.getBitmap(this.contentResolver, pickedPhoto)
-                            binding.pictureField.setImageBitmap(pickedBitMap)
-                        }
-             */
         }
         super.onActivityResult(requestCode, resultCode, data)
     }
@@ -157,11 +145,10 @@ class AddNote : AppCompatActivity() {
             val source = ImageDecoder.createSource(this.contentResolver, uri!!)
             val pickedBitMap = ImageDecoder.decodeBitmap(source)
             binding.pictureField.setImageBitmap(pickedBitMap)
-            binding.etTemp.setText(uri.toString())
+            pictureUri = uri.toString()
             return pickedBitMap;
         } else {
             val pickedBitMap = MediaStore.Images.Media.getBitmap(this.contentResolver, uri)
-            //binding.pictureField.setImageBitmap(pickedBitMap)
             return pickedBitMap
         }
     }
--- app/src/main/java/com/example/notes/MainActivity.kt
+++ app/src/main/java/com/example/notes/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.Window
 import android.widget.LinearLayout
 import android.widget.PopupMenu
 import androidx.activity.result.contract.ActivityResultContracts
@@ -35,6 +36,8 @@ class MainActivity : AppCompatActivity(),  NotesAdapter.NotesItemClickListener, 
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        supportActionBar?.hide()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
