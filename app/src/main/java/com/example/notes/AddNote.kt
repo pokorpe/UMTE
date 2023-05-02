c3aca5a6849e889a3e6de5aee95b9321bb12c38f
pokorpe <117550576+pokorpe@users.noreply.github.com>
2023-05-02 04:50:17 +0200

    Title color bugfix

--- app/src/main/java/com/example/notes/AddNote.kt
+++ app/src/main/java/com/example/notes/AddNote.kt
@@ -4,17 +4,14 @@ import android.app.Activity
 import android.content.Intent
 import android.graphics.Color
 import android.icu.text.SimpleDateFormat
-import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.example.notes.data.db.entities.NoteEntity
 import com.example.notes.databinding.ActivityAddNoteBinding
-import com.example.notes.databinding.ActivityMainBinding
 import com.nvt.color.ColorPickerDialog
 import java.lang.Exception
 import java.util.*
-import java.util.logging.SimpleFormatter
 
 class AddNote : AppCompatActivity() {
 
@@ -48,41 +45,30 @@ class AddNote : AppCompatActivity() {
             val colorPicker = ColorPickerDialog(
                 this,
                 Color.BLACK, // color init
-                true, // true is show alpha
+                false, // true is show alpha
                 object : ColorPickerDialog.OnColorPickerListener {
                     override fun onCancel(dialog: ColorPickerDialog?) {
-                        // handle click button Cancel
                     }
 
-                    //-65536 red
                     override fun onOk(dialog: ColorPickerDialog?, colorPicker: Int) {
-                        // onBackPressed()
-                        Toast.makeText(this@AddNote, colorPicker.toString(), Toast.LENGTH_SHORT).show()
-                        //binding.etTitle.setTextColor(colorPicker)
-                        //newtc = colorPicker
-                        //binding.etTitle.setTextColor(colorPicker)
+                        //Toast.makeText(this@AddNote, colorPicker.toString(), Toast.LENGTH_SHORT).show()
                         val title = binding.etTitle.text.toString()
-                        val note_desc = binding.etNote.text.toString()
+                        val noteDesc = binding.etNote.text.toString()
                         val tc = binding.etTitle.currentTextColor
-                        if (title.isNotEmpty() || note_desc.isNotEmpty()) {
-                            val formatter = SimpleDateFormat("EEE, d MMM yyyy HH:mm")
-                            if (isUpdated) {
-                                note = NoteEntity(
-                                    old_note.id, title, note_desc, formatter.format(Date()), colorPicker
-                                )
-                            } else {
-                                note = NoteEntity(
-                                    null, title, note_desc, formatter.format(Date()), colorPicker
-                                )
-                            }
-                            val intent = Intent()
-                            intent.putExtra("note", note)
-                            setResult(Activity.RESULT_OK, intent)
-                            finish()
+                        val formatter = SimpleDateFormat("EEE, d MMM yyyy HH:mm")
+                        if (isUpdated) {
+                            note = NoteEntity(
+                                old_note.id, title, noteDesc, old_note.date, colorPicker
+                            )
                         } else {
-                            Toast.makeText(this@AddNote, "Prosím vložte text", Toast.LENGTH_SHORT).show()
+                            note = NoteEntity(
+                                null, title, noteDesc, formatter.format(Date()), colorPicker
+                            )
                         }
-
+                        val intent = Intent()
+                        intent.putExtra("note", note)
+                        setResult(Activity.RESULT_OK, intent)
+                        binding.etTitle.setTextColor(colorPicker)
                     }
                 })
             colorPicker.show()
@@ -90,23 +76,19 @@ class AddNote : AppCompatActivity() {
 
         binding.imgCheck.setOnClickListener {
             val title = binding.etTitle.text.toString()
-            val note_desc = binding.etNote.text.toString()
+            val noteDesc = binding.etNote.text.toString()
             val tc = binding.etTitle.currentTextColor
-            if (title.isNotEmpty() || note_desc.isNotEmpty()) {
+            if (title.isNotEmpty() || noteDesc.isNotEmpty()) {
                 val formatter = SimpleDateFormat("EEE, d MMM yyyy HH:mm")
                 if (isUpdated) {
                     note = NoteEntity(
-                        old_note.id, title, note_desc, formatter.format(Date()), tc
+                        old_note.id, title, noteDesc, old_note.date, tc
                     )
                 } else {
                     note = NoteEntity(
-                        null, title, note_desc, formatter.format(Date()), tc
+                        null, title, noteDesc, formatter.format(Date()), tc
                     )
                 }
-                /*
-                if (tC!= null) {
-                    binding.etTitle.setTextColor(tC)
-                }*/
                 val intent = Intent()
                 intent.putExtra("note", note)
                 setResult(Activity.RESULT_OK, intent)
